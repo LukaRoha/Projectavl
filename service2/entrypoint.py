5ff9ad9c9bb52b660750d31bf5478ffd6169a6b0
LukaRoha <122794856+LukaRoha@users.noreply.github.com>
2023-01-18 11:01:49 +0100

    Update entrypoint.py

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -5,7 +5,7 @@ from sys import request
 
 SERVICE1_URL = "http://service.example.com:8080"
 
-message = request.get( SERVICE1_URL, sys.stdin.readline()).text
+message = request.get(sys.stdin.readline()).text
 data = ["md5", message]
 
 print(request.post(SERVICE1_URL, data="\n".join(data)))
